ee66b28b6034a1b82d88ecc3777962caa8acf02c
jatin mishra <jatinmishra2094011@gmail.com>
2026-01-30 13:00:28 +0530

    new updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,41 @@
-import React from "react";
-import { FaShoppingCart, FaUser, FaSearch } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaShoppingCart, FaUser, FaSearch, FaBars, FaTimes } from "react-icons/fa";
 import "./Header.css";
-import { Link, NavLink } from "react-router-dom"; 
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <header className="header">
       <div className="header-container">
+        {/* Hamburger Menu Icon (Mobile Only) */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          {isMenuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+
         {/* Logo Section */}
         <div className="logo">
           CLOTH<span>STORE</span>
         </div>
 
-        {/* Navigation Links */}
-       <nav className="nav">
-  <Link to="/" className="nav-link">Home</Link>
-  <Link to="/about" className="nav-link">About Us</Link>
-  <NavLink to="/men" className="nav-link">Men</NavLink>
-  <NavLink to="/women" className="nav-link">Women</NavLink>
-  <Link to="/kids" className="nav-link">Kids</Link>
-  <Link to="/sale" className="nav-link sale">Sale</Link>
-</nav>
+        {/* Navigation Links - Added 'active' class based on state */}
+        <nav className={`nav ${isMenuOpen ? "nav-active" : ""}`}>
+          <Link to="/" className="nav-link" onClick={() => setIsMenuOpen(false)}>Home</Link>
+          <Link to="/about" className="nav-link" onClick={() => setIsMenuOpen(false)}>About Us</Link>
+          <NavLink to="/men" className="nav-link" onClick={() => setIsMenuOpen(false)}>Men</NavLink>
+          <NavLink to="/women" className="nav-link" onClick={() => setIsMenuOpen(false)}>Women</NavLink>
+          <Link to="/kids" className="nav-link" onClick={() => setIsMenuOpen(false)}>Kids</Link>
+          <Link to="/sale" className="nav-link sale" onClick={() => setIsMenuOpen(false)}>Sale</Link>
+        </nav>
 
         {/* Action Icons & Search */}
         <div className="right">
-          <div className="search-box">
+          <div className="search-box mobile-hide">
             <FaSearch className="search-icon" />
-            <input type="text" placeholder="Search for brands, styles..." />
+            <input type="text" placeholder="Search..." />
           </div>
 
           <div className="icon-group">
